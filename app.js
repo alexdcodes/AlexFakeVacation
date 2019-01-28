a3ba3436477eabe9621ac35395543d283cd2e723
Alex Diker <alex.diker@live.ca>
2019-01-27 20:14:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.post('/process', function(req, res){
     console.log('Form (from querystring): ' + req.query.form);
     console.log('CSRF token (from hidden form field): ' + req.body._csrf);
     console.log('Name (from visible form field): ' + req.body.name);
-
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
 });
 
 app.get('/', function(req, res) {
